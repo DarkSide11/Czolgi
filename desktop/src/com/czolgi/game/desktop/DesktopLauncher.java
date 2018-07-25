b5ae25abe502b875d532877d488bca850c236ca1
DarkSide11 <31825497+DarkSide11@users.noreply.github.com>
2018-07-25 23:41:58 +0200

    init
    init

---
+++ desktop/src/com/czolgi/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.czolgi.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.czolgi.game.Czolgi;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Czolgi(), config);
+	}
+}
